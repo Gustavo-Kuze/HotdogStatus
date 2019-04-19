39803f45a4a364cd39990e8411cf358144fbd7c5
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-04-19 11:25:13 -0300

    Estilização

---
+++ firebaseSetup.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyCswFFkLfQpPy8FwweCzOeBrJrHukNxJBw",
+    authDomain: "hotdogstatus-2ab23.firebaseapp.com",
+    databaseURL: "https://hotdogstatus-2ab23.firebaseio.com",
+    projectId: "hotdogstatus-2ab23",
+    storageBucket: "hotdogstatus-2ab23.appspot.com",
+    messagingSenderId: "581234406799"
+};
+
+firebase.initializeApp(config);
